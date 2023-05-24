442e032e2873852cb335e67c5070231e66bcf863
GabiStz <gabriele.steinmetz1901@gmail.com>
2023-05-24 18:52:37 -0300

    Arrumando controller movimentacao

--- src/main/java/br/com/uniamerica/estacionamento/controller/MovimentacaoController.java
+++ src/main/java/br/com/uniamerica/estacionamento/controller/MovimentacaoController.java
@@ -1,91 +1,86 @@
-package br.com.uniamerica.estacionamento.controller;
+package br.com.uniamerica.Estacionamentopedro.controller;
 
-import br.com.uniamerica.estacionamento.entity.Condutor;
-import br.com.uniamerica.estacionamento.entity.Movimentacao;
-import br.com.uniamerica.estacionamento.entity.Veiculo;
-import br.com.uniamerica.estacionamento.repository.MovimentacoRepository;
-import br.com.uniamerica.estacionamento.service.CondutorService;
-import br.com.uniamerica.estacionamento.service.MovimentacaoService;
+import br.com.uniamerica.Estacionamentopedro.entity.Movimentacao;
+import br.com.uniamerica.Estacionamentopedro.entity.Veiculo;
+import br.com.uniamerica.Estacionamentopedro.service.MovimentacaoService;
+import br.com.uniamerica.Estacionamentopedro.service.VeiculoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.time.Duration;
 
 @RestController
-@RequestMapping(value = "/api/movimentacao")
+@RequestMapping(value = "api/movimentacoes")
 public class MovimentacaoController {
-    @Autowired
-    private MovimentacoRepository movimentacoRepository;
     @Autowired
     private MovimentacaoService movimentacaoService;
 
-
     @GetMapping("/{id}")
-    public ResponseEntity<?> findById(@PathVariable("id") final Long id) {
-        try {
-            Movimentacao movimentacao = movimentacaoService.findById(id);
-            return ResponseEntity.ok("Objeto encontrado ");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("ERROR!" + e.getMessage());
-        }
+    public ResponseEntity<?> findById(@PathVariable("id") final Long id){
+        final Movimentacao movimentacao = this.movimentacaoService.findById(id);
+        return movimentacao == null
+                ? ResponseEntity.badRequest().body("Nenhum valor encontrado.")
+                : ResponseEntity.ok(movimentacao);
     }
 
-    @GetMapping("/ListaCompleta")
-    public ResponseEntity<?> listaC() {
-        try {
-            return ResponseEntity.ok().body(movimentacaoService.findAll());
-        }
-        catch (Exception e){
-            return ResponseEntity.badRequest().body("ERROR" + e.getMessage());
-        }
+    @GetMapping("/lista")
+    public ResponseEntity<?> listaCompleta(){
+        return ResponseEntity.ok(this.movimentacaoService.listaCompleta());
     }
 
+    @GetMapping("/lista/ativos")
+    public ResponseEntity<?> listaAtivos(){
+        return ResponseEntity.ok(this.movimentacaoService.listaMovimentacoesAtivas());
+    }
 
     @PostMapping
-    public ResponseEntity<?> cadastrar(@RequestBody final Movimentacao movimentacao) {
+    public ResponseEntity<?> cadastrar(@RequestBody final Movimentacao movimentacao){
         try {
-            this.movimentacaoService.cadastrar(movimentacao);
-            return ResponseEntity.ok("Cadastrado com sucesso");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("ERROR");
+            return ResponseEntity.ok(movimentacaoService.cadastrar(movimentacao));
+        }
+        catch (DataIntegrityViolationException e){
+            return ResponseEntity.internalServerError()
+                    .body("Error: " + e.getCause().getCause().getMessage());
         }
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<?> atualizar(@PathVariable("id") final Long id, @RequestBody Movimentacao movimentacao) {
-        Movimentacao movimentacaoBanco = this.movimentacaoService.findById(id);
-        movimentacaoBanco.setEntrada(movimentacaoBanco.getEntrada());
-        movimentacaoBanco.setSaida(movimentacaoBanco.getSaida());
-        movimentacaoBanco.setTempo(movimentacaoBanco.getTempo());
-        movimentacaoBanco.setTempoMulta(movimentacaoBanco.getTempoMulta());
-        movimentacaoBanco.setTempoDesconto(movimentacaoBanco.getTempoDesconto());
-        movimentacaoBanco.setValorHora(movimentacaoBanco.getValorHora());
-        movimentacaoBanco.setValorHoraMulta(movimentacaoBanco.getValorHoraMulta());
-        movimentacaoBanco.setValorMulta(movimentacaoBanco.getValorMulta());
-        movimentacaoBanco.setCadastro(movimentacaoBanco.getCadastro());
+    @PutMapping("/{idMovimentacao}")
+    public ResponseEntity<?> atualizar(
+            @PathVariable Long idMovimentacao,
+            @RequestBody Movimentacao movimentacao
+    ) {
         try {
-            this.movimentacaoService.atualizar(movimentacao);
-            return ResponseEntity.ok("Atualizado com Sucesso");
-
+            this.movimentacaoService.atualizar(idMovimentacao, movimentacao);
+            return ResponseEntity.ok().body("Movimentacao atualizada com sucesso!");
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
-        catch (Exception e){
-            return ResponseEntity.badRequest().body("ERROR");
-        }
-
     }
-    @DeleteMapping
-    public ResponseEntity<?>deletar(@PathVariable("id") final Long id){
-        Movimentacao movimentacaoBanco= this.movimentacaoService.findById(id);
-        try {
-            this.movimentacaoService.delete(movimentacaoBanco);
-            return ResponseEntity.ok("Deletado com sucesso");
-        }
-        catch (Exception e){
-            return ResponseEntity.badRequest().body("ERROR");
-        }
 
+
+    @PutMapping("/desativar/{idMovimentacao}")
+    public ResponseEntity<?> desativar(
+            @PathVariable Long idMovimentacao
+    ){
+        try{
+            this.movimentacaoService.desativar(idMovimentacao);
+            return ResponseEntity.ok().body("Veiculo desativado com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/ativar/{idMovimentacao}")
+    public ResponseEntity<?> ativar(
+            @PathVariable Long idMovimentacao
+    ){
+        try{
+            this.movimentacaoService.ativar(idMovimentacao);
+            return ResponseEntity.ok().body("Movimentacao ativada com sucesso!");
+        }catch (RuntimeException e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
